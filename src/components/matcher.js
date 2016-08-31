aadc1e9c6bf52a8bd116f6ccb9c00f870ce10ebd
camille morell <cemorell>
2016-08-31 11:59:13 -0500

    maybe this time

--- src/components/matcher.js
+++ src/components/matcher.js
@@ -1,37 +1,66 @@
 import React from 'react';
 
-class Requester extends React.Component {
+class Matcher extends React.Component {
 
     constructor(props){
-      super(props);
-      this.state= {
-        matched:{},
-      }
+        super(props);
+        this.state= {
+            matchedUser:{},
+            currentUser: {}
+        }
+    }
+
+    _getOtherUserId( request, user ) {
+        return request.sender_id != user._id ? request.sender_id : request.recipient_id;
     }
 
     componentWillMount(){
-      //with that request object, go get the sending user
-      $.ajax({
-        method: "GET",
-        url: "/users/" + this.props.object.sender_id,
-        dataType: 'json'
-      })
-      .done(function(data){
-        console.log(data)
-        this.setState({matched: data[0]});
-      }.bind(this))
-      .fail(function(error){
-        console.log(error);
-      })
+        //with that request object, go get the sending user
+        $.ajax({
+                url: '/profile',
+                method: "GET",
+                dataType: "json"
+            })
+            .done(function(data){
+                this.setState({currentUser: data});
+                var other_user_id = this._getOtherUserId( this.props.object, data );
+                this.fetchOtherUser( other_user_id );
+            }.bind(this))
+            .fail(function(error){
+                console.log(error);
+            })
     }
 
-  render(){
-      return (
-        <div className="matcher">
-          <h2> { this.state.matched._id } </h2>
-        </div>
+
+
+    fetchOtherUser( other_user_id )
+    {
+
+        $.ajax({
+                method: "GET",
+                url: "/users/" + other_user_id,
+                dataType: 'json'
+            })
+            .done(function(data){
+                console.log(data);
+                this.setState({matchedUser: data[0]});
+            }.bind(this))
+            .fail(function(error){
+                console.log(error);
+            })
+
+    }
+
+
+
+
+    render(){
+        return (
+            <div className="matcher">
+                <h2> { this.state.matchedUser.nickname } </h2>
+            </div>
         );
-      }
+    }
 }
 
-export default Requester;
+export default Matcher;
